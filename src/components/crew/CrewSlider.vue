7d6e75bcb415ea716e037d1616740f7ceb5eddad
Malik Elbadri <elbadrimalik@gmail.com>
2022-07-22 21:04:22 +0300

    crew slider added

--- src/components/crew/CrewSlider.vue
+++ src/components/crew/CrewSlider.vue
@@ -1,12 +1,84 @@
 <template>
   <div class="crew-slider">
-    <h1>CREW SLIDER</h1>
+    <div class="slider-left">
+      <h2>
+        {{ $store.state.selectedCrew.role }}
+      </h2>
+      <h1>
+        {{ $store.state.selectedCrew.name }}
+      </h1>
+      <p>
+        {{ $store.state.selectedCrew.bio }}
+      </p>
+      <SliderController />
+    </div>
+
+    <div class="slider-right">
+      <img
+        :src="
+          require(`../../assets/crew/${$store.state.selectedCrew.images.png}`)
+        "
+        alt="ve"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import SliderController from "./SliderController.vue";
+export default {
+  components: { SliderController },
+};
 </script>
 
-<style>
+<style scoped>
+.crew-slider {
+  display: flex;
+  align-items: center;
+  max-height: 100%;
+}
+.slider-left {
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+}
+h1,
+h2,
+p {
+  margin: 0px;
+}
+
+.slider-left > h2 {
+  font-family: "Bellefair";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 37px;
+  text-transform: uppercase;
+  color: #ffffff;
+  mix-blend-mode: normal;
+  opacity: 0.5;
+}
+
+.slider-left > h1 {
+  font-family: "Bellefair";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 56px;
+  line-height: 64px;
+  text-transform: uppercase;
+  color: #ffffff;
+}
+.slider-left > p {
+  font-family: "Barlow";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 32px;
+  color: #d0d6f9;
+}
+
+.slider-right {
+  position: relative;
+}
 </style>
---
+++ src/components/crew/SliderController.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="slider-controller">
+    <div
+      class="controller-item"
+      :class="{ active: items[0] }"
+      @click="toggleItem(0)"
+    ></div>
+    <div
+      class="controller-item"
+      :class="{ active: items[1] }"
+      @click="toggleItem(1)"
+    ></div>
+    <div
+      class="controller-item"
+      :class="{ active: items[2] }"
+      @click="toggleItem(2)"
+    ></div>
+    <div
+      class="controller-item"
+      :class="{ active: items[3] }"
+      @click="toggleItem(3)"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [true, false, false, false],
+    };
+  },
+  methods: {
+    toggleItem(index) {
+      for (let i = 0; i < this.items.length; i++) {
+        this.items[i] = false;
+      }
+      this.items[index] = true;
+      this.$store.state.selectedCrew = this.$store.state.crew[index];
+    },
+  },
+};
+</script>
+
+<style>
+.slider-controller {
+  display: flex;
+  gap: 20px;
+  margin-top: 88px;
+}
+
+.controller-item {
+  border-radius: 100%;
+  height: 15px;
+  width: 15px;
+  background: #ffffff;
+  mix-blend-mode: normal;
+  opacity: 0.17;
+}
+.controller-item.active {
+  opacity: 1;
+}
+</style>
